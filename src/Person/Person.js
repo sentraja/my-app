f41a5180daa63e784633f8ede23d6308450f24c5
senthil Raja <info.sentraja@gmail.com>
2020-05-26 01:08:33 +0530

    learning with the props, and the props. childeren, also the parent div for grouping more than single tag and so on

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,12 @@ import React from 'react';
 const persons = (props) => {
     // return  <p> I am a person and I am {Math.floor(Math.random()* 30)} years old</p>
 
-    return <p> I am {props.name} and I am {props.age} years old</p>
+    return (
+        <div>
+            <p> I am {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
+    )
 
 };
 
