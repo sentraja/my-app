f37b6a7bb8b5aa1e83f96259f2532e87f98526b6
senthil Raja <info.sentraja@gmail.com>
2020-05-26 00:42:42 +0530

    Added a new compoennt with fucntional component and how to include the new component and other things

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Person1 from './Person/Person'; // Person1 should be any name since it get the refernce form the path and it should be in the name we used to create. Then the same namespace has to use at the refernece to include the components here.
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1> Hi! I am a react app...</h1>
-      <p> This is awesome... We can have only one root div here. we can't have another root div.</p>
-      <p> which is commented below you can see it.</p>
+        <h1> Hi! I am a react app...</h1>
+        <p> This is awesome... We can have only one root div here. we can't have another root div.</p>
+        <p> which is commented below you can see it.</p>
+
+
+        <Person1 />
+        {/* The first character should be in capital to make difference from the reserved HTML keyword like div - Div and so on. */}
+
       </div>
-      // <h2>Hi am h2</h2>
+      
     );
     // return React.createElement('div',{className:  'App'},React.createElement('h1',null,'I am a react developer'));
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// created a fucntional component with JSX elemnts and hence we import the react as line no: 1
+// Here we didn't import component in first line, since we didnt use the class that extends the component
+const persons = () => {
+return  <p>This is a person!!!</p>
+};
+
+export default persons
+
+
+// Creating a functional component.
