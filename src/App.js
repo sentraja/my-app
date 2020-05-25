05eb94d7340bcb1a62c6dd380567cf370fe6d98c
senthil Raja <info.sentraja@gmail.com>
2020-05-26 00:16:17 +0530

    only one root div can hold multiple nested divs and which can helps to deign the elements with root class name. Here it is App.

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ class App extends Component {
     return (
       <div className="App">
       <h1> Hi! I am a react app...</h1>
-      
+      <p> This is awesome... We can have only one root div here. we can't have another root div.</p>
+      <p> which is commented below you can see it.</p>
       </div>
+      // <h2>Hi am h2</h2>
     );
     // return React.createElement('div',{className:  'App'},React.createElement('h1',null,'I am a react developer'));
   }
