5ad040049af89b7af8e548597be0b476f3d58100
senthil Raja <info.sentraja@gmail.com>
2020-05-26 00:10:14 +0530

    The JSX format looks like HTML but really it is not. its a JS and using syntatical sugar which is JSX

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //   <h1> Hi! I am a react app...</h1>
+    return (
+      <div className="App">
+      <h1> Hi! I am a react app...</h1>
       
-    //   </div>
-    // );
-    return React.createElement('div',{className:  'App'},React.createElement('h1',null,'I am a react developer'));
+      </div>
+    );
+    // return React.createElement('div',{className:  'App'},React.createElement('h1',null,'I am a react developer'));
   }
 }
 
